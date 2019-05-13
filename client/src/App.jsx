f7626eda188e6690e03ae0220badd5b1004af6b4
Andrew Ngo <andrewtruongngo@gmail.com>
2019-05-12 18:02:22 -0700

    Include react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
 	constructor(props) {
@@ -6,7 +7,11 @@ class App extends React.Component {
 	}
 
 	render() {
-		return <div>Hello Andrew</div>;
+		return (
+			<HashRouter basename="/">
+				<div>Hello Andrew</div>
+			</HashRouter>
+		);
 	}
 }
 
