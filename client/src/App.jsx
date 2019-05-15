5cc59572afa173d93787329ad9cb566b2f3fb696
Andrew Ngo <andrewtruongngo@gmail.com>
2019-05-14 21:05:19 -0700

    Include hashrouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<HashRouter basename="https://andrewtruongngo.github.io/">
+			<HashRouter basename="https://andrewtruongngo.github.io/site">
 				<div>
 					<div>Hello Andrew</div>
 				</div>
