09f465a8f4c998f5a4a46c1dff3838aac32011ba
Andrew Ngo <andrewtruongngo@gmail.com>
2019-05-14 21:09:16 -0700

    Remove 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
-import Page404 from './pages/Page404';
 
 class App extends React.Component {
 	constructor(props) {
